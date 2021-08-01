6f4ffe0d9d747ab277d5124e25d277acbf90a49e
Mykola Kuzyk <ukrboy99@gmail.com>
2021-08-01 23:37:09 +0200

    Add project files.

---
+++ Safe_App_inheritance/JewelThief.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Safe_App_inheritance
+{
+    class JewelThief: Locksmith
+    {
+        private string stolenJewels;
+        protected void ReturnContents (string safeContets, SafeOwner owner)
+        {
+            stolenJewels = safeContets;
+            Console.WriteLine($"I`m stealing the jewels! I stole: {stolenJewels}");
+        }
+    }
+}
---
+++ Safe_App_inheritance/Locksmith.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Safe_App_inheritance
+{
+    class Locksmith
+    {
+        public void OpenSafe(Safe safe, SafeOwner owner)
+        {
+            safe.PickLock(this);
+            string safeContents = safe.Open(Combination);
+            ReturnContents(safeContents, owner);
+        }
+        public string Combination { private get; set; }
+        protected void ReturnContents(string safeContents, SafeOwner owner)
+        {
+            owner.ReceiveContents(safeContents);
+        }
+    }
+}
